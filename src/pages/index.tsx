b2f59cfaaf3e82b1251ffe565c8f357e80f0c407
Victor Bruno <victorbrunof@icloud.com>
2022-10-29 16:15:29 -0300

    update config

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 8,
+  border: 0,
+  padding: "4px 8px",
+});
+
+export default function Home() {
+  return <Button>Enviar</Button>;
+}
